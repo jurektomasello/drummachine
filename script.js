00addfc9019262e281f4fa982db24a06153a633f
Jurek <jurek.tomasello@gmail.com>
2019-02-12 09:02:52 +0100

    Add event listener for bass drum

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ let box;
 for (var i = 0; i < 8; i++) {
 	box = document.createElement('div');
 	box.className = 'box';
-	flex.appendChild(box);
+	container.appendChild(box);
 }
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 32) {
+		let bass = document.getElementById('bass-drum');
+		bass.play();
+		bass.currentTime = 0;
+	}
+})
